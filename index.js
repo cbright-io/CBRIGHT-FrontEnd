e464622cd85208da87c7196391a94cd3b1f1be66
TheAngryEnglishman <whorlowharry@gmail.com>
2022-02-25 19:05:28 +0100

    scroll magic

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 const root = document.documentElement;
-const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue(
-    "--marquee-elements-displayed"
-);
-const marqueeContent = document.querySelector("ul.marquee-content");
+ticker1();
+ticker2();
 
-root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+function ticker1() {
+    const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue(
+        "--marquee-elements-displayed"
+    );
+    const marqueeContent = document.querySelector("ul.marquee-content");
 
-for (let i = 0; i < marqueeElementsDisplayed; i++) {
-    marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+    root.style.setProperty(
+        "--marquee-elements",
+        marqueeContent.children.length
+    );
+
+    for (let i = 0; i < marqueeElementsDisplayed; i++) {
+        marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+    }
 }
 
-const marqueeStrategyElementsDisplayed = getComputedStyle(
-    root
-).getPropertyValue("--marquee-strategy-elements-displayed");
+function ticker2() {
+    const marqueeStrategyElementsDisplayed = getComputedStyle(
+        root
+    ).getPropertyValue("--marquee-strategy-elements-displayed");
 
-const marqueeStrategyContent = document.querySelector(
-    "ul.marquee-strategy-content"
-);
-
-root.style.setProperty(
-    "--marquee-strategy-elements",
-    marqueeStrategyContent.children.length
-);
-
-for (let i = 0; i < marqueeStrategyElementsDisplayed; i++) {
-    marqueeStrategyContent.appendChild(
-        marqueeStrategyContent.children[i].cloneNode(true)
+    const marqueeStrategyContent = document.querySelector(
+        "ul.marquee-strategy-content"
     );
+
+    root.style.setProperty(
+        "--marquee-strategy-elements",
+        marqueeStrategyContent.children.length
+    );
+
+    for (let i = 0; i < marqueeStrategyElementsDisplayed; i++) {
+        marqueeStrategyContent.appendChild(
+            marqueeStrategyContent.children[i].cloneNode(true)
+        );
+    }
 }
