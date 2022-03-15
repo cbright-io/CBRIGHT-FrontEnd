e9a7a46cdde8128a6ec03b9347e74c1f19bd180d
TheAngryEnglishman <whorlowharry@gmail.com>
2022-03-15 14:01:58 +0100

    glide update

---
+++ sub_pages/aboutUs.js
@@ -0,0 +1,20 @@
+import Glide from "@glidejs/glide";
+
+window.addEventListener("load", () => {
+    const glide = new Glide(".glide", {
+        type: "carousel",
+        touchAngle: 45,
+        focusAt: 1,
+        startAt: 1,
+        perView: 5.5,
+        keyboard: true,
+        gap: 50,
+        autoplay: false,
+        peek: {
+            before: 100,
+            after: 50,
+        },
+    });
+
+    glide.mount();
+});
