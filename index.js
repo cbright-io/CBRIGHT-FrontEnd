5fd40dc5731529c0f444534bab793de3790d93b9
TheAngryEnglishman <whorlowharry@gmail.com>
2022-03-24 10:16:40 +0100

    glide import update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// import Glide from "./node_modules/@amcharts/amcharts4/maps";
+import Glide from "./node_modules/@amcharts/amcharts4/maps";
 
 let vw;
 let cardsNo;
@@ -6,24 +6,24 @@ resize();
 
 alert("Site under development");
 
-// var glideMulti1 = new Glide(".multi1", {
-//     type: "carousel",
-//     autoplay: 3500,
-//     perView: 7,
-// });
+var glideMulti1 = new Glide(".multi1", {
+    type: "carousel",
+    autoplay: 3500,
+    perView: 7,
+});
 
-// glideMulti1.mount();
+glideMulti1.mount();
 
 function resize() {
     vw = Math.max(
         document.documentElement.clientWidth || 0,
         window.innerWidth || 0
     );
+
     if (vw < 800) {
         cardsNo = 5;
     } else if (vw < 1300) {
         cardsNo = 7;
-        console.log("switch");
     } else {
         cardsNo = 11;
     }
