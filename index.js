05420804c903df9e033c45dc983043c3999c8894
TheAngryEnglishman <whorlowharry@gmail.com>
2022-03-28 10:30:38 +0200

    marquee scroll return, about us resize started

--- index.js
+++ index.js
@@ -26,3 +26,15 @@ function resize() {
         cardsNo = 11;
     }
 }
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue(
+    "--marquee-elements-displayed"
+);
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for (let i = 0; i < marqueeElementsDisplayed; i++) {
+    marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
