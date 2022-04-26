65bf02d6a44c284692a44fdcc0ea0acb6912dfa0
harry whorlow <whorlowharry@gmail.com>
2022-04-26 11:48:45 +0200

    webpack update

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import Glide from "@glidejs/glide";
+
+let vw;
+let cardsNo;
+resize();
+
+var glideMulti1 = new Glide(".multi1", {
+    type: "carousel",
+    autoplay: 3500,
+    perView: 9,
+});
+
+glideMulti1.mount();
+
+function resize() {
+    vw = Math.max(
+        document.documentElement.clientWidth || 0,
+        window.innerWidth || 0
+    );
+
+    if (vw < 800) {
+        cardsNo = 5;
+    } else if (vw < 1300) {
+        cardsNo = 7;
+    } else {
+        cardsNo = 11;
+    }
+}
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue(
+    "--marquee-elements-displayed"
+);
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for (let i = 0; i < marqueeElementsDisplayed; i++) {
+    marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
